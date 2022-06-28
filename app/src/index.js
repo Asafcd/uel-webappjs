74a948ec09f807db03f65fed0592ddb01464325c
Asafcd <asafcontreras@gmail.com>
2022-06-27 23:10:39 -0500

    config fix

--- app/src/index.js
+++ app/src/index.js
@@ -1,17 +1,38 @@
 // CONST
 const express = require("express");
-const ejs = require("ejs")
-const app = express();
+const cukip = require("cookie-parser")
+const exhbs = require('express-handlebars')
 const path = require("path");
+const morgan = require('morgan')
+
+const app = express();
 
 //SETTINGS
-app.set("port", 80);
+app.set("port", process.env.PORT || 8082);
 app.set("views", path.join(__dirname, "views"));
-app.engine("html", require("ejs").renderFile);
-app.set("view engine", "ejs");
+app.engine(".hbs", exhbs.engine({
+  extname: '.html',
+  defaultLayout:'main',
+  layoutsDir: path.join(app.get('views'), 'layouts'),
+  partialsDir: path.join(app.get('views'), 'partials'),
+  helpers: require('./lib/handlebars')
+  }));
+app.set("view engine", "hbs");
+
+//middlewares
+app.use(morgan('dev'))
+app.use(express.urlencoded({extended:false}))
+app.use(express.json())
+
+//GLOBALS
+app.use((req,res,next)=>{
+  next()
+})
 
 //ROUTES
-app.use(require("./routes/views"));
+app.use(require("./routes/index"));
+app.use(require('./routes/authentication'))
+app.use('noticia',require('./routes/noticia'))
 
 //STATIC FILES
 app.use(express.static(path.join(__dirname, "public/static")));
