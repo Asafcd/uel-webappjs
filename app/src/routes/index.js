797d9fccaa3d8ecb61a8be26479916bb5d2a5b58
Asafcd <asafcontreras@gmail.com>
2022-08-03 22:25:19 -0500

    front almost done

--- app/src/routes/index.js
+++ app/src/routes/index.js
@@ -3,12 +3,21 @@ const router = express.Router();
 const pool = require('../database')
 
 //#region GUESTS
-router.get('/', async (req, res) => {
+router.get('/', async (req, res) => {    
     try{
-        let datamundial = await pool.query("SELECT * FROM noticias WHERE etiqueta='NoticiasMundiales'")
-        //console.log(data[0])
-        res.render('home.hbs', {data:datamundial});  
-    }catch(err){console.log(err)}  
+        let mundial = await pool.query("SELECT * FROM noticias WHERE etiqueta='mundial' ORDER BY id_noticia DESC LIMIT 4")  
+        let local = await pool.query("SELECT * FROM noticias WHERE etiqueta='local' ORDER BY id_noticia DESC LIMIT 4")  
+        let deportes = await pool.query("SELECT * FROM noticias WHERE etiqueta='deportes' ORDER BY id_noticia DESC LIMIT 4")  
+        let gastro = await pool.query("SELECT * FROM noticias WHERE etiqueta='gastronomia' ORDER BY id_noticia DESC LIMIT 4")  
+        let kids = await pool.query("SELECT * FROM noticias WHERE etiqueta='kids' ORDER BY id_noticia DESC LIMIT 4")  
+        let tec = await pool.query("SELECT * FROM noticias WHERE etiqueta='tecnologia' ORDER BY id_noticia DESC LIMIT 4")  
+        let arte = await pool.query("SELECT * FROM noticias WHERE etiqueta='arte y cultura' ORDER BY id_noticia DESC LIMIT 4")  
+        let ambi = await pool.query("SELECT * FROM noticias WHERE etiqueta='medio ambiente' ORDER BY id_noticia DESC LIMIT 4")          
+        res.render('home.hbs', {
+            mundial, local, deportes, gastro, kids, tec, arte, ambi
+            });
+        }          
+    catch(err){console.log(err)}  
 });
 
 router.get('/nosotros', (req, res) => {    
--- app/src/routes/noticias.js
+++ app/src/routes/noticias.js
@@ -2,35 +2,78 @@ const express = require('express')
 const router = express.Router();
 const pool = require('../database')
 
-//#region Router GETS
+//#region Router categories GETS
 router.get('/mundiales', async (req,res)=>{
     try{
-        let data = pool.query("SELECT * FROM noticias WHERE etiqueta = 'NoticiasMundiales'")
+        let data = pool.query("SELECT * FROM noticias WHERE etiqueta = 'mundial' ORDER BY id_noticia DESC")
         res.render('noticias/mundiales.hbs', {data:data})
     }catch(err) {console.log(err)}
     
 });
 router.get('/locales', (req,res)=>{
-    res.render('noticias/locales.hbs')
+    try {
+        let data = pool.query("SELECT * FROM noticias WHERE etiqueta = 'local' ORDER BY id_noticia DESC")
+        res.render('noticias/locales.hbs',{data})
+    } catch (error) { console.log(error) }
+    
 });
-router.get('/deportes', (req,res)=>{
-    res.render('noticias/deportes.hbs')
+router.get('/deportes', async (req,res)=>{
+    try {
+        let data = await pool.query(
+            "SELECT n.*, u.nombres as username, u.apellidos as userlasts FROM noticias n JOIN usuarios u WHERE n.etiqueta='deportes' AND n.id_usuario = u.id_usuario ORDER BY n.id_noticia DESC")
+        //let fuentes = await poo
+        console.log(data)
+        res.render('noticias/deportes.hbs',{data})
+    } catch (error) { console.log(error) }
+    
 });
 router.get('/arte', (req,res)=>{
-    res.render('noticias/arte.hbs')
+    try {
+        let data = pool.query("SELECT * FROM noticias WHERE etiqueta = 'arte y cultura' ORDER BY id_noticia DESC")
+        res.render('noticias/arte.hbs', {data:data})
+    } catch (error) { console.log(error) }
+    
 });
 router.get('/desarrollo', (req,res)=>{
-    res.render('noticias/desarrollo.hbs')
+    try {
+        let data = pool.query("SELECT * FROM noticias WHERE etiqueta = 'tecnologia' ORDER BY id_noticia DESC")
+        res.render('noticias/desarrollo.hbs',{data})
+    } catch (error) { console.log(error) }
+    
 });
 router.get('/medioambiente', (req,res)=>{
-    res.render('noticias/medioambiente.hbs')
+    try {
+        let data = pool.query("SELECT * FROM noticias WHERE etiqueta = 'medio ambiente' ORDER BY id_noticia DESC")
+        res.render('noticias/medioambiente.hbs',{data})
+    } catch (error) { console.log(error) }
+    
 });
 router.get('/gastronomia', (req,res)=>{
-    res.render('noticias/gastronomia.hbs')
+    try {
+        let data = pool.query("SELECT * FROM noticias WHERE etiqueta = 'gastronomia' ORDER BY id_noticia DESC")
+        res.render('noticias/gastronomia.hbs',{data})
+    } catch (error) { console.log(error) }
+    
 });
 router.get('/kids', (req,res)=>{
-    res.render('noticias/kids.hbs')
+    try {
+        let data = pool.query("SELECT * FROM noticias WHERE etiqueta = 'kids' ORDER BY id_noticia DESC")
+        res.render('noticias/kids.hbs',{data})
+    } catch (error) { console.log(error) }
+    
 });
 //#endregion
 
+//#region verNoticia 
+/*router.get('/vernoticia/:id', async (req,res)=>{
+    let id = req.params
+    console.log(id)
+    try{
+        let data = pool.query("SELECT * FROM noticias WHERE etiqueta = 'mundial' ORDER BY id_noticia DESC")
+        res.render('noticias/mundiales.hbs', {data:data})
+    }catch(err) {console.log(err)}
+    
+});
+*/
+//#endregion
 module.exports = router;
