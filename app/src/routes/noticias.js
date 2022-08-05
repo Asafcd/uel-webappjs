b6f2f5f3eeb0878630a38a62e1edf3cc0a754d15
Asafcd <asafcontreras@gmail.com>
2022-08-05 14:38:47 -0500

    user role system at 80%

--- app/src/routes/noticias.js
+++ app/src/routes/noticias.js
@@ -3,20 +3,6 @@ const router = express.Router();
 const pool = require('../database')
 
 //#region Router categories GETS
-router.get('/mundiales', async (req,res)=>{
-    try{
-        let data = pool.query("SELECT * FROM noticias WHERE etiqueta = 'mundial' ORDER BY id_noticia DESC")
-        res.render('noticias/mundiales.hbs', {data:data})
-    }catch(err) {console.log(err)}
-    
-});
-router.get('/locales', (req,res)=>{
-    try {
-        let data = pool.query("SELECT * FROM noticias WHERE etiqueta = 'local' ORDER BY id_noticia DESC")
-        res.render('noticias/locales.hbs',{data})
-    } catch (error) { console.log(error) }
-    
-});
 router.get('/:tag', async (req,res)=>{
     let {tag} = req.params
     let etiq = {tag}
@@ -24,49 +10,15 @@ router.get('/:tag', async (req,res)=>{
         let tag = await pool.query("SELECT * FROM etiquetas")
         let data = await pool.query(
             "SELECT n.*, u.nombres as username, u.apellidos as userlasts, f.nombre as fuente, f.link FROM noticias n \
-            JOIN usuarios u JOIN fuentes f WHERE n.etiqueta=? AND n.id_usuario = u.id_usuario AND n.id_fuente = f.id_fuente \
+            JOIN usuarios u JOIN fuentes f JOIN etiquetas e WHERE e.id=? AND n.id_usuario = u.id_usuario AND n.id_fuente = f.id_fuente \
             ORDER BY n.id_noticia DESC", [etiq.tag])
         //let fuentes = await poo
         //console.log(data[0])
         res.render('noticias/templateSeccion.hbs',{data,tag})
     } catch (error) { console.log(error) }
-    
+    ""
 });
-router.get('/arte', (req,res)=>{
-    try {
-        let data = pool.query("SELECT * FROM noticias WHERE etiqueta = 'arte y cultura' ORDER BY id_noticia DESC")
-        res.render('noticias/arte.hbs', {data:data})
-    } catch (error) { console.log(error) }
-    
-});
-router.get('/desarrollo', (req,res)=>{
-    try {
-        let data = pool.query("SELECT * FROM noticias WHERE etiqueta = 'tecnologia' ORDER BY id_noticia DESC")
-        res.render('noticias/desarrollo.hbs',{data})
-    } catch (error) { console.log(error) }
-    
-});
-router.get('/medioambiente', (req,res)=>{
-    try {
-        let data = pool.query("SELECT * FROM noticias WHERE etiqueta = 'medio ambiente' ORDER BY id_noticia DESC")
-        res.render('noticias/medioambiente.hbs',{data})
-    } catch (error) { console.log(error) }
-    
-});
-router.get('/gastronomia', (req,res)=>{
-    try {
-        let data = pool.query("SELECT * FROM noticias WHERE etiqueta = 'gastronomia' ORDER BY id_noticia DESC")
-        res.render('noticias/gastronomia.hbs',{data})
-    } catch (error) { console.log(error) }
-    
-});
-router.get('/kids', (req,res)=>{
-    try {
-        let data = pool.query("SELECT * FROM noticias WHERE etiqueta = 'kids' ORDER BY id_noticia DESC")
-        res.render('noticias/kids.hbs',{data})
-    } catch (error) { console.log(error) }
-    
-});
+
 //#endregion
 
 //#region verNoticia 
--- app/src/routes/user.js
+++ app/src/routes/user.js
@@ -1,123 +1,218 @@
-const express = require('express')
+const express = require("express");
 const router = express.Router();
-const pool = require('../database')
+const pool = require("../database");
 
 //#region GETS
-router.get('/borradores', async (req,res) =>{
-    try {
-        let data = await pool.query("SELECT * FROM noticias WHERE id_usuario=1 AND estado='Borrador'")
-        //console.log(data)
-        res.render('user/noticias.hbs',{data})
-    } catch (error) {console.log(error)}    
-})
-router.get('/enviadas', async (req,res) =>{
-    try {
-        let data = await pool.query("SELECT * FROM noticias WHERE id_usuario=1 AND estado='Enviada'")
-        //console.log(data)
-        res.render('user/noticias.hbs',{data})
-    } catch (error) {console.log(error)}
-    
-})
-router.get('/aceptadas', async (req,res) =>{
-    try {
-        let data = await pool.query("SELECT * FROM noticias WHERE id_usuario=1 AND estado='Aceptada'")
-        //console.log(data)
-        res.render('user/noticias.hbs',{data})
-    } catch (error) {console.log(error)}
-    
-})
+router.get("/borradores", async (req, res) => {
+  try {
+    let data = await pool.query(
+      "SELECT * FROM noticias WHERE estado='Borrador'"
+    );
+    //console.log(data)
+    res.render("user/noticias.hbs", { data });
+  } catch (error) {
+    console.log(error);
+  }
+});
+router.get("/enviadas", async (req, res) => {
+  try {
+    let data = await pool.query(
+      "SELECT * FROM noticias WHERE id_usuario=1 AND estado='Enviada'"
+    );
+    //console.log(data)
+    res.render("user/noticias.hbs", { data });
+  } catch (error) {
+    console.log(error);
+  }
+});
+router.get("/aceptadas", async (req, res) => {
+  try {
+    let data = await pool.query(
+      "SELECT * FROM noticias WHERE id_usuario=1 AND estado='Aceptada'"
+    );
+    //console.log(data)
+    res.render("user/noticias.hbs", { data });
+  } catch (error) {
+    console.log(error);
+  }
+});
 //MENSAJES
-router.get('/buzon', async (req,res) =>{
-    //const {id_noticia} = req.params
-    //let id = {id_noticia}
-    try {
-        let data = await pool.query("SELECT * FROM noticias WHERE id_usuario=1")
-        let datamensajes = await pool.query("SELECT * FROM mensajes m JOIN noticias n WHERE m.id_noticia = n.id_noticia")   
-        let mensajes = []
-        data.forEach(e => {
-            datamensajes.forEach( m=>{
-                if(e.id_noticia==m.id_noticia){
-                    mensajes.push({
-                        id_mensaje: m.id_mensaje,
-                        id_noticia: m.id_noticia,
-                        contenido: m.contenido,
-                        titulo: m.titulo
-                    })
-            }
-            })            
-        });  
-        res.render('user/buzon.hbs', {mensajes})
-    } catch (error) { console.log(error) }
-})
-router.get('/vermensaje/:id_mensaje', async (req,res) =>{
-    const {id_mensaje} = req.params
-    let mensaje = {id_mensaje}
-    try {
-        let data = await pool.query("SELECT * FROM mensajes WHERE id_mensaje=?",[mensaje.id_mensaje])
-        let noticia = await pool.query("SELECT titulo, contenido, id_usuario FROM noticias WHERE id_noticia=?", [data[0].id_noticia])
-        let autor = await pool.query("SELECT nombres, apellidos FROM usuarios WHERE id_usuario=?",[noticia[0].id_usuario])
-        console.log(autor)
-        res.render('user/vermensaje.hbs', {data:data[0], noticia:noticia[0], autor:autor[0]})  
+router.get("/buzon", async (req, res) => {
+  //const {id_noticia} = req.params
+  //let id = {id_noticia}
+  try {
+    let data = await pool.query("SELECT * FROM noticias WHERE id_usuario=1");
+    let datamensajes = await pool.query(
+      "SELECT * FROM mensajes m JOIN noticias n WHERE m.id_noticia = n.id_noticia"
+    );
+    let mensajes = [];
+    data.forEach((e) => {
+      datamensajes.forEach((m) => {
+        if (e.id_noticia == m.id_noticia) {
+          mensajes.push({
+            id_mensaje: m.id_mensaje,
+            id_noticia: m.id_noticia,
+            contenido: m.contenido,
+            titulo: m.titulo,
+          });
+        }
+      });
+    });
+    res.render("user/buzon.hbs", { mensajes });
+  } catch (error) {
+    console.log(error);
+  }
+});
+router.get("/vermensaje/:id_mensaje", async (req, res) => {
+  const { id_mensaje } = req.params;
+  let mensaje = { id_mensaje };
+  try {
+    let data = await pool.query("SELECT * FROM mensajes WHERE id_mensaje=?", [
+      mensaje.id_mensaje,
+    ]);
+    let noticia = await pool.query(
+      "SELECT titulo, contenido, id_usuario FROM noticias WHERE id_noticia=?",
+      [data[0].id_noticia]
+    );
+    let autor = await pool.query(
+      "SELECT nombres, apellidos FROM usuarios WHERE id_usuario=?",
+      [noticia[0].id_usuario]
+    );
+    console.log(autor);
+    res.render("user/vermensaje.hbs", {
+      data: data[0],
+      noticia: noticia[0],
+      autor: autor[0],
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
 
-        } catch (error) { console.log(error) }
-})
+router.get("/vernoticia/:id_noticia", async (req, res) => {
+  const { id_noticia } = req.params;
+  let id = { id_noticia };
+  console.log(id);
+  try {
+    let data = await pool.query("SELECT * FROM noticias WHERE id_noticia=?", [ id.id_noticia ]);
+    console.log(data[0])
+    let fuente = await pool.query("SELECT nombre, link FROM fuentes WHERE id_fuente=?", [data[0].id_fuente] );
+    let autor = await pool.query(
+      "SELECT id_usuario, nombres, apellidos FROM usuarios WHERE id_usuario=?",
+      [data[0].id_usuario]
+    );
+    res.render("user/vernoticia.hbs", {
+      data: data[0],
+      fuente,
+      autor: autor[0],
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
 
-router.get('/vernoticia/:id_noticia', async (req,res) =>{
-    const {id_noticia} = req.params
-    let id = {id_noticia}
-    console.log(id)
-    try {
-        let data = await pool.query("SELECT * FROM noticias WHERE id_noticia=?",[id.id_noticia])
-        let fuente = await pool.query("SELECT nombre, link FROM fuentes WHERE id_noticia=?",[id.id_noticia])
-        let autor = await pool.query("SELECT id_usuario, nombres, apellidos FROM usuarios WHERE id_usuario=?",[data[0].id_usuario])
-        console.log(fuente)
-        res.render('user/vernoticia.hbs', {data:data[0], fuente, autor:autor[0]})
-    } catch (error) { console.log(error) }
-})
+router.get("/crearnoticia", async (req, res) => {
+  try {
+    let fuentes = await pool.query("SELECT * FROM fuentes");
+    let users = await pool.query(
+      "SELECT id_usuario, nombres, apellidos FROM usuarios"
+    );
+    let tag = await pool.query("SELECT * FROM etiquetas");
 
-router.get('/crearnoticia', async (req,res) =>{
-    try {
-        let users = await pool.query("SELECT id_usuario, nombres, apellidos FROM usuarios")
-        //console.log(users)
-        res.render('user/crearnoticia.hbs', {users})
-    } catch (error) { console.log(error) }
-})
+    res.render("user/crearnoticia.hbs", { users, tag, fuentes });
+  } catch (error) {
+    console.log(error);
+  }
+});
 
-router.get('/editnoticia/:id_noticia', async (req,res) =>{
-    const {id_noticia} = req.params
-    let id = {id_noticia}
-    try {
-        let data = await pool.query("SELECT * FROM noticias WHERE id_noticia=?",[id.id_noticia])
-        let users = await pool.query("SELECT id_usuario, nombres, apellidos FROM usuarios WHERE id_usuario=?",[data[0].id_usuario])
-        console.log(users)
-        res.render('user/crearnoticia.hbs', {users, data})
-    } catch (error) { console.log(error) }
-})
-router.get('/deletenoticia/:id_noticia', async (req,res) =>{
-    const {id_noticia} = req.params
-    let id = {id_noticia}
-    try {
-        await pool.query("DELETE FROM noticias WHERE id_noticia=?",[id.id_noticia])
-        res.redirect('/user/borradores')
-    } catch (error) { console.log(error) }
-})
-router.get('/enviar/:id_noticia', async (req,res) =>{
-    const {id_noticia} = req.params
-    let id = {id_noticia}
-    try {
-        await pool.query("UPDATE noticias SET estado='Enviada' WHERE id_noticia=?",[id.id_noticia])
-        res.redirect('/user/borradores')
-    } catch (error) { console.log(error) }
-})
+router.get("/editnoticia/:id_noticia", async (req, res) => {
+  const { id_noticia } = req.params;
+  let id = { id_noticia };
+  try {
+    let data = await pool.query("SELECT * FROM noticias WHERE id_noticia=?", [
+      id.id_noticia,
+    ]);
+    let users = await pool.query(
+      "SELECT id_usuario, nombres, apellidos FROM usuarios WHERE id_usuario=?",
+      [data[0].id_usuario]
+    );
+    console.log(users);
+    res.render("user/crearnoticia.hbs", { users, data });
+  } catch (error) {
+    console.log(error);
+  }
+});
+router.get("/deletenoticia/:id_noticia", async (req, res) => {
+  const { id_noticia } = req.params;
+  let id = { id_noticia };
+  try {
+    await pool.query("DELETE FROM noticias WHERE id_noticia=?", [
+      id.id_noticia,
+    ]);
+    res.redirect("/user/borradores");
+  } catch (error) {
+    console.log(error);
+  }
+});
+router.get("/enviar/:id_noticia", async (req, res) => {
+  const { id_noticia } = req.params;
+  let id = { id_noticia };
+  try {
+    await pool.query(
+      "UPDATE noticias SET estado='Enviada' WHERE id_noticia=?",
+      [id.id_noticia]
+    );
+    res.redirect("/user/borradores");
+  } catch (error) {
+    console.log(error);
+  }
+});
 //#endregion
 //#region POSTS
 
-/*router.post('/crearnoticia', async (req,res) =>{
-    let {titulo, contenido, estado, etiqueta, autor, fuentenombre, fuentelink, mainimg, extraimg} = req.body
-    let noticianew = {titulo, contenido, estado, etiqueta, autor, fuentenombre, fuentelink, mainimg, extraimg}
-    
-    res.send(noticianew)
-})*/
+router.post("/crearnoticia", async (req, res) => {
+  let { titulo, contenido, estado, etiqueta,
+    autor, fuente, link, mainimg, extraimg,  } = req.body;
+  let noticianew = { titulo, contenido, estado, etiqueta,
+    autor, fuente, link, mainimg, extraimg, };
+
+    console.log(noticianew.link)
+  //Crear entidades desde el add form, solo si no existen y no son strings vacios
+  let aa = noticianew.fuente
+  let aabool = false;
+  let fuentes = await pool.query("SELECT * FROM fuentes");
+
+  fuentes.forEach((a) => {
+    if (aa.indexOf(a.nombre) !== -1 || aa==="") {
+      aabool = true;
+    }    
+  });
+  if (!aabool) {
+    try {
+      await pool.query( "INSERT INTO fuentes(nombre, link) VALUES(?,?)", [noticianew.fuente, noticianew.link] );
+    } catch (err) { console.log(err); }
+  }  
+    let fuenteid = await pool.query("SELECT id_fuente FROM fuentes WHERE nombre = ?",[noticianew.fuente])
+    //console.log(fuenteid[0].id_fuente)
+  try {
+    await pool.query(
+      "INSERT INTO noticias(id_usuario,id_fuente,titulo,contenido,estado,etiqueta,main_image,extra_image1) VALUES(?,?,?,?,?,?,?,?)",
+      [
+        noticianew.autor,
+        fuenteid[0].id_fuente,
+        noticianew.titulo,
+        noticianew.contenido,
+        noticianew.estado,
+        noticianew.etiqueta,
+        noticianew.mainimg,
+        noticianew.extraimg,
+      ]
+    );
+    res.redirect("/user");
+  } catch (e) {
+    console.log(e);
+  }
+});
 //#endregion
 
 module.exports = router;
